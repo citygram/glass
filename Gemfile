9184a4edd374634ba63cd3cf9f7b40e542f771f5
Danny Whalen <daniel.r.whalen@gmail.com>
2014-09-15 14:47:51 -0400

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'https://rubygems.org'
+
+gem 'dotenv'
+gem 'puma'
+gem 'sinatra'
+gem 'spy_glass', github: 'codeforamerica/spy_glass', ref: '70ab647c984'
---
+++ app.rb
@@ -0,0 +1,30 @@
+require 'dotenv';Dotenv.load
+require 'sinatra'
+require 'spy_glass'
+
+proxy = SpyGlass::Proxy.new ENV.fetch('HOST') do |config|
+  config.content_type = 'application/json'
+  config.define_parser { |body| JSON.parse(body) }
+  config.define_generator { |body| JSON.pretty_generate(body) }
+
+  config.cache = ActiveSupport::Cache::MemoryStore.new(expires_in: Integer(ENV['CACHE_TTL'] || 5.minutes))
+
+  config.define_transformation do |collection|
+    features = collection.map do |record|
+      longitude = record['longitude'] || (record['point'] && record['point']['longitude'])
+      latitude = record['latitude'] || (record['point'] && record['point']['latitude'])
+
+      { 'type' => 'Feature',
+        'properties' => record,
+        'geometry' => {
+          'type' => 'Point',
+          'coordinates' => [
+            longitude.to_f,
+            latitude.to_f ] } }
+    end
+
+    { 'type' => 'FeatureCollection', 'features' => features }
+  end
+end
+
+get('*', provides: :json, &proxy)
